500513c4f7f3bd9cdb542e1e9fd4e39e1ff895b8
Vitaliy Semenov <synthex@bk.ru>
2023-11-19 19:58:28 +0600

    chore: install illuminate/support

--- app/Controllers/CollectionController.php
+++ app/Controllers/CollectionController.php
@@ -4,22 +4,30 @@ declare(strict_types=1);
 
 namespace Synthex\Phptherightway\Controllers;
 
-use Synthex\Phptherightway\Core\Collection\Collection;
+
 
 class CollectionController
 {
     public function index()
     {
-        $employees = Collection::make([
-            ['name' => 'John', 'email' => 'j@j.j', 'salaried' => true],
-            ['name' => 'Jane', 'email' => 'j@j.j', 'salaried' => false],
-            ['name' => 'Jack', 'email' => 'j@j.j', 'salaried' => true],
-        ]);
+        $filePath =  __DIR__ . '/../../storage/json/products.json';
 
-        $numberOfSalariedEmployees = $employees
-            ->filter(fn(array $employee) => $employee['salaried'])
-            ->count();
+        $productsJson = json_decode(file_get_contents($filePath), true);
 
-        echo 'Number of salaried employees: ' . $numberOfSalariedEmployees;
+        $products = collect($productsJson['products']);
+
+        $totalCost = 0;
+
+        foreach ($products as $product) {
+            $productType = $product['product_type'];
+
+            if ($productType === 'Lamp' || $productType === 'Wallet') {
+                foreach ($product['variants'] as $productVariant) {
+                    $totalCost += $productVariant['price'];
+                }
+            }
+        }
+
+        echo $totalCost;
     }
 }
